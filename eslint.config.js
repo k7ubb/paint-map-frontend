3702cbfef80b43ef6a8d24368301e1be5da6bcff
k7ubb <64680945+k7ubb@users.noreply.github.com>
2025-09-11 17:53:56 +0900

    eslintを導入

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig({
+	files: ['**/*.js'],
+	plugins: { js },
+	extends: ['js/recommended'],
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			...globals.node,
+			L: 'readonly'
+		}
+	},
+	rules: {
+		'semi': ['error', 'always'],
+		'quotes': ['error', 'single'],
+		'indent': ['error', 'tab']
+	}
+});
